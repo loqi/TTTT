1e2408f68abe39ffaa1c9f475efbbd1cf922610f
Park Loqi <loqi@cruzio.com>
2022-01-27 01:21:05 -0800

    still fiddling with directory structure

--- tttt/_clade.py
+++ tttt/_clade.py
@@ -1,10 +1,10 @@
 class Tttt:
     nib = "~^"
 
-    def unwad():
+    def obj__wad():
         # Return TtttRequest from wad ( -guid, -hmac, unpadded, decrypted, uncompressed, parsed )
 
-    def wadup():
+    def wad__obj():
         # Return wad ( serialized, compressed, encrypted, padded, +hmac, +guid ) from TtttRequest
 
 # recurse into clade subdirectories and add their APIs to this base API.
@@ -14,12 +14,12 @@ class Tttt:
 # request = tttt.unpack(udp, ip6, datagram)
 # "~^@$ PUSH 12.280 @$b8F6sG to uR6vFL92 via [ rir6W7eN GpY+Bzc6 LpR20sVu qQHz1w7P ] from G7zR+Ftx fare 0.281 need 0.186"
 # TtttRequest object =
-#       { _source: "G7zR+Ftx"
+#       { _ret: "G7zR+Ftx"
 #       , _nib:  "~^@$"
 #       , _yarn: "@$b8F6sG"
 #       , verb:  "PUSH"
-#       , amt:   12280
-#       , of:    millikudos
+#       , sum:  12280
+#       , of:   "millikudos"
 #       , to:   "uR6vFL92"
 #       , from: "G7zR+Ftx"
 #       , fare: 0.281
