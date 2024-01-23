2eaf4da93d0909afe1cdfe5456042c6166153736
Park Loqi <parkloqi@gmail.com>
2024-01-22 21:24:30 -0800

    merge conflict fixed

--- experiment/crypt.py
+++ experiment/crypt.py
@@ -1,39 +1,38 @@
-<<<<<<< HEAD
-from Crypto.PublicKey import ECC
-from Crypto.Hash import SHAKE128
-from Crypto.Protocol.DH import key_agreement
+# from Crypto.PublicKey import ECC
+# from Crypto.Hash import SHAKE128
+# from Crypto.Protocol.DH import key_agreement
 
-# This KDF has been agreed in advance
-def kdf(x):
-        return SHAKE128.new(x).read(32)
+# # This KDF has been agreed in advance
+# def kdf(x):
+#         return SHAKE128.new(x).read(32)
 
-# In a real scenario, this long-term key already exists
-U_static = ECC.generate(curve='p256')
+# # In a real scenario, this long-term key already exists
+# U_static = ECC.generate(curve='p256')
 
-# This ephemeral key is generated only for this session
-U_ephemeral = ECC.generate(curve='p256')
+# # This ephemeral key is generated only for this session
+# U_ephemeral = ECC.generate(curve='p256')
 
-# In a real scenario, this long-term key is received from the peer
-# and it is verified as authentic
-V_static = ECC.generate(curve='p256').public_key()
+# # In a real scenario, this long-term key is received from the peer
+# # and it is verified as authentic
+# V_static = ECC.generate(curve='p256').public_key()
 
-# In a real scenario, the peer generated this ephemeral key only
-# for this session. It doesn't need to be authenticated if the
-# static key of V already has been.
-V_ephemeral = ECC.generate(curve='p256').public_key()
+# # In a real scenario, the peer generated this ephemeral key only
+# # for this session. It doesn't need to be authenticated if the
+# # static key of V already has been.
+# V_ephemeral = ECC.generate(curve='p256').public_key()
 
-session_key = key_agreement( static_priv=U_static
-                           , static_pub =V_static
-                           , eph_priv   =U_priv
-                           , eph_pub    =U_pub
-                           , kdf        =kdf
-                           )
+# session_key = key_agreement( static_priv=U_static
+#                            , static_pub =V_static
+#                            , eph_priv   =U_priv
+#                            , eph_pub    =U_pub
+#                            , kdf        =kdf
+#                            )
 
-# session_key is an AES-256 key, which will be used to encrypt
-# subsequent communications
+# # session_key is an AES-256 key, which will be used to encrypt
+# # subsequent communications
 
-session_key
-=======
+# session_key
+
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
@@ -66,4 +65,3 @@ derived_key_2 = HKDF( algorithm=hashes.SHA256()
                     ).derive(shared_key_2)
 
 peer_public_key
->>>>>>> c6d843c4d01d1432917e7e085d29ce905685ae22
